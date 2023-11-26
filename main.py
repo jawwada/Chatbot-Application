418f1a40d1f7d1ca7c1f18e5bbbc39cb5c505b16
Ahmed Jawad <jawwada@gmail.com>
2023-11-26 16:28:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,5 +11,5 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Hi I am running and will got to new york')
+    print_hi('Hello World!')
 
